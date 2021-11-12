a451ee3effda73e9b4a2e3dbe077e21465a48419
Murray Williams <murray@waterfalldigital.co.za>
2021-11-12 16:04:16 +0200

    Logged in user avatar to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // styles & images
 import './Sidebar.css'
@@ -6,12 +7,19 @@ import DashboardIcon from '../assets/dashboard_icon.svg'
 import AddIcon from '../assets/add_icon.svg'
 
 export default function Sidebar() {
+  const { user } = useAuthContext()
+
+  console.log(user)
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          {/* {avatar of user} */}
-          <p>Hey user</p>
+          {user && (
+            <>
+              <img src={user.photoURL} alt={user.displayName} />
+              <p>Hey {user.displayName}</p>
+            </>
+          )}
         </div>
         <nav className="links">
           <ul>
