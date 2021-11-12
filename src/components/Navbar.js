4ed875379ddcb6c84c044f27e151304fea39d872
Murray Williams <murray@waterfalldigital.co.za>
2021-11-12 15:46:43 +0200

    Hide/show navbar links based on auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // styles & images
 import './Navbar.css'
@@ -7,6 +8,7 @@ import Template from '../assets/temple.svg'
 
 export default function Navbar() {
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
 
   return (
     <div className="navbar">
@@ -17,25 +19,30 @@ export default function Navbar() {
             <span>The Dojo</span>
           </Link>
         </li>
-
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button onClick={logout} className="btn">
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button disabled className="btn">
-              Logging out...
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button onClick={logout} className="btn">
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button disabled className="btn">
+                Logging out...
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </div>
   )
