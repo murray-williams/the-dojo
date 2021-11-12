70593a13ea5b1cb961f83265514be8553c554408
Murray Williams <murray@waterfalldigital.co.za>
2021-11-12 08:42:52 +0200

    Setup signup form + basic styles

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,49 @@
+import { useState } from 'react'
+
 // styles
 import './Signup.css'
 
 export default function Signup() {
-  return <div>Signup</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+
+  return (
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>profile thumbnail:</span>
+        <input required type="file" />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  )
 }
