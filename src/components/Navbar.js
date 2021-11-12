566cee09344bf15c8df53696c9250537c9d66e4b
Murray Williams <murray@waterfalldigital.co.za>
2021-11-12 15:09:02 +0200

    Hook up logout method + update onlien status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 // styles & images
 import './Navbar.css'
 import Template from '../assets/temple.svg'
 
 export default function Navbar() {
+  const { logout, isPending } = useLogout()
+
   return (
     <div className="navbar">
       <ul>
@@ -22,7 +25,16 @@ export default function Navbar() {
           <Link to="/signup">Signup</Link>
         </li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && (
+            <button onClick={logout} className="btn">
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button disabled className="btn">
+              Logging out...
+            </button>
+          )}
         </li>
       </ul>
     </div>
