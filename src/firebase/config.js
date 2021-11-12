488330875546c2b9fe2c3665921ff9f8e5bbe00f
Murray Williams <murray@waterfalldigital.co.za>
2021-11-12 09:33:15 +0200

    Setup firebase storage service

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDy6ggC15yONWkO-SzFXmxA2_-a8ryA9Q4',
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig)
 // init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp }
