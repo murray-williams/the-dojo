cf6c96abd9819974a22325528144c1106918dd97
Murray Williams <murray@waterfalldigital.co.za>
2021-11-18 14:49:00 +0200

    Fetch project details using document hook

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { projectFirestore } from '../firebase/config'
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  // realtime data for document
+
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id)
+    // get realtime updates on document
+    // snapshot is the document in the firestore db
+    // if anything on the doc changes the function fires again
+    // and we get the updated snapshot with the changes
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        // use data() method to get the data inside snapshot
+        setDocument({ ...snapshot.data(), id: snapshot.id })
+        setError(null)
+
+        // second agument that runs if there is an error
+      },
+      (err) => {
+        console.log(err.message)
+        setError('failed to get document')
+      }
+    )
+    // unsubscribe if component unmounts
+    return () => unsubscribe()
+  }, [collection, id])
+
+  return { document, error }
+}
